fe8f5546ce6ebfd4ca95b6837bd76e972791c0d0
Michael Silberstein <mikesilb123@gmail.com>
2017-12-28 21:43:56 -0500

    Added 'Blackjack' constructor with passing test

--- BlackJackTDD/Program.cs
+++ BlackJackTDD/Program.cs
@@ -6,8 +6,40 @@ using System.Collections.Generic;
 
 namespace BlackJackTDD
 {
-    class MainClass
+    public class Blackjack
     {
+        private Dictionary<string, Deck> gameDataDeck = new Dictionary<string, Deck>();
+        public Dictionary<string, Hand> gameDataHand = new Dictionary<string, Hand>();
+        public Dictionary<string, List<int>> gameDataListInt = new Dictionary<string, List<int>>();
+        public Dictionary<string, string> gameDataString = new Dictionary<string, string>();
+
+        public Blackjack()
+        {
+            Deck gameDeck = new Deck();
+            gameDataDeck.Add("Game Deck", gameDeck);
+            List<Card> playerHandList = new List<Card> { };
+            List<Card> dealerHandList = new List<Card> { };
+            Hand playerHand = new Hand(playerHandList);
+            Hand dealerHand = new Hand(dealerHandList);
+            gameDataHand.Add("Player Hand", playerHand);
+            gameDataHand.Add("Dealer Hand", dealerHand);
+            List<int> playerScore = new List<int> {  };
+            List<int> dealerScore = new List<int> {  };
+            gameDataListInt.Add("Player Score", playerScore);
+            gameDataListInt.Add("Dealer Score", dealerScore);
+            playerScore.Add(0);
+            dealerScore.Add(0);
+            gameDataString.Add("Win Lose or Draw", "");
+            string computerBustOutput = "";
+        }
+
+        public Dictionary<string, Deck> GetGameDataDeck()
+        {
+            return gameDataDeck;
+        }
+
+
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
